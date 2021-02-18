bbbb8adbb7c2c7eebe36d2278a59c6f497f3e514
franckalainai <franckalain.ai@gmail.com>
2021-02-18 08:16:36 +0000

    recover UserController.php

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -53,21 +53,8 @@ class UserTransformer extends TransformerAbstract
 
     public static function originalAttribute($index){
         $attributes = [
-            'identifier' => 'id',
-            'name' => 'name',
-            'email' => 'email',
-            'isVerified' => 'verified',
-            'isAdmin' => 'admin',
-            'creationDate' => 'created_at',
-            'lastChange' => 'updated_at',
-            'deletedDate' => 'deleted_at'
-        ];
 
-        return isset($attributes[$index]) ? $attributes[$index] : null;
-    }
 
-    public static function transformedAttribute($index){
-        $attributes = [
             'id' =>'identifier',
             'name' =>'name',
             'email'=>'email',
@@ -76,6 +63,22 @@ class UserTransformer extends TransformerAbstract
             'created_at' => 'creationDate',
             'updated_at' => 'lastChange',
             'deleted_at' => 'deletedDate'
+        ];
+
+        return isset($attributes[$index]) ? $attributes[$index] : null;
+    }
+
+    public static function transformedAttribute($index){
+        $attributes = [
+
+            'identifier' => 'id',
+            'name' => 'name',
+            'email' => 'email',
+            'isVerified' => 'verified',
+            'isAdmin' => 'admin',
+            'creationDate' => 'created_at',
+            'lastChange' => 'updated_at',
+            'deletedDate' => 'deleted_at'
         ];
 
         return isset($attributes[$index]) ? $attributes[$index] : null;
