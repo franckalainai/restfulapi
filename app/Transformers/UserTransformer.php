dd540bacd0aa998eb625603472d5284702d575d3
franckalainai <franckalain.ai@gmail.com>
2021-02-18 08:54:46 +0000

    updated user transformer

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -55,8 +55,6 @@ class UserTransformer extends TransformerAbstract
 
     public static function originalAttribute($index){
         $attributes = [
-<<<<<<< HEAD
-=======
             'identifier' => 'id',
             'name' => 'name',
             'email' => 'email',
@@ -67,22 +65,6 @@ class UserTransformer extends TransformerAbstract
             'creationDate' => 'created_at',
             'lastChange' => 'updated_at',
             'deletedDate' => 'deleted_at'
-        ];
->>>>>>> 13b55c8111d8607e3d525fb6a46c33e3e52c502a
-
-
-
-
-            'id' =>'identifier',
-            'name' =>'name',
-            'email'=>'email',
-            'verified' =>'isVerified',
-            'admin' => 'isAdmin',
-            'password' => 'password',
-            'password_confirmation' => 'password_confirmation',
-            'created_at' => 'creationDate',
-            'updated_at' => 'lastChange',
-            'deleted_at' => 'deletedDate'
         ];
 
         return isset($attributes[$index]) ? $attributes[$index] : null;
@@ -96,6 +78,8 @@ class UserTransformer extends TransformerAbstract
             'email' => 'email',
             'isVerified' => 'verified',
             'isAdmin' => 'admin',
+            'password' => 'password',
+            'password_confirmation' => 'password_confirmation',
             'creationDate' => 'created_at',
             'lastChange' => 'updated_at',
             'deletedDate' => 'deleted_at'
